a23b73d999c934182b3a6c17e2d303db266ab266
mayambacode <bokambma@sheridancollege.ca>
2024-12-03 22:24:23 -0500

    First Commit

---
+++ app/src/main/java/project/stn991578659/moviemate/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package project.stn991578659.moviemate.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
